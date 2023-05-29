d04ae21a42b0301cea644740bfd0c3440b1b2c01
Erhan Ertem <eertem@gmail.com>
2023-05-29 12:26:02 +0300

    Create barebone tw project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./**/*.{html,js}'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Josefin Sans', 'sans-serif'],
+				alata: ['Alata'],
+			},
+		},
+	},
+	plugins: [],
+};
